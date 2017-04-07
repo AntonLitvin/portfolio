f51424f85cd4481ab16e63b7beab5904c73ee1ae
Anton Litvinenko <anton8383@mail.ru>
2017-04-07 12:27:07 +0300

    upload portfolio

---
+++ js/common.js
@@ -0,0 +1,120 @@
+$(window).load(function() { 
+	$('.loader-inner').fadeOut(); 
+	$('.loader').delay(400).fadeOut('slow'); 
+
+	$('.hidden-menu a').animated('fadeInUp', 'fadeInUp');
+	$('.subject-name').animated('fadeInDown', 'fadeOut');
+	$('.header-name p').animated('fadeInUp', 'fadeOut');
+	$('.section-header').animated('fadeInUp', 'fadeOut');
+	$('.services-item:nth-child(3),.services-item:nth-child(4)').animated('fadeInRight', 'fadeOut');
+	$('.my-photo-wrapper').animated('flipInY', 'fadeOut');
+	$('.about-me-left').animated('fadeInLeft', 'fadeOut');
+	$('.about-me-right').animated('fadeInRight', 'fadeOut');
+	$('.work-item').animated('fadeInLeft', 'fadeOut');
+	$('.study-item').animated('fadeInRight', 'fadeOut');
+});
+
+
+$(function() {
+
+
+/* fix hover in portfolio menu
+===============================*/
+$('.portfolio-item').on('click', function(){
+	$('.portfolio-item-descr').removeAttr('style');
+	$(this).find('.portfolio-item-descr').css({'opacity':'1', 'height':'100%'});
+});
+
+$('.portfolio-item-descr a').on('click', function(e){
+	e.stopPropagation();
+	$(this).parent('.portfolio-item-descr').css({'opacity':'0', 'height':'0'});
+});
+
+$('.portfolio-item').on('mouseover', function(){
+	$('.portfolio-item-descr').removeAttr('style');
+});
+
+
+/* Resize header height
+===============================*/
+function heightDetect() {
+	$('.main-header').css('height', $(window).height() + 2);
+};
+
+heightDetect();
+
+$(window).resize(function() {
+	heightDetect();
+});
+
+
+/* Sandwich button and menu
+===============================*/
+$('.menu-toggle').on('click', function() {
+	$('.sandwich').toggleClass('active');
+})
+
+$('.hidden-menu a').on('click', function() {
+	$('.hidden-menu').fadeOut();
+	$('.sandwich').removeClass('active');
+})
+
+$('.menu-toggle').on('click', function() {
+	$('.hidden-menu').fadeToggle();
+	$('.top-menu a').toggleClass('fadeInUp animated');
+})
+
+
+/* Scroll to block
+===============================*/
+$('.hidden-menu a').on('click', function () {
+
+	var topNavHeight = $('.navbar-custom').innerHeight();
+	var elementClick = $(this).attr("href");
+	var destination = $(elementClick).offset().top;
+
+	$('html, body').animate({ scrollTop: destination + 5 }, 800);
+	return false; 
+});
+
+
+/* to-top button
+===============================*/
+$(window).scroll(function() {
+	if($(this).scrollTop() > 100) {
+		$('.to-top-btn').fadeIn();
+	} else {
+		$('.to-top-btn').fadeOut();
+	}
+});
+
+$('.to-top-btn').on('click', function() {
+	$('body, html').animate({ scrollTop:0 },800);
+	if ($('.hidden-menu').is(':visible')) {
+		$('.hidden-menu').fadeOut();
+	}
+	return false;
+});
+
+
+/* MixItUp
+===============================*/
+$('.portfolio-wrap').mixItUp();
+
+
+/* Set Active menu item
+===============================*/
+$('.portfolio-nav a').on('click', function(){
+	$('.portfolio-nav a').removeClass('active');
+	$(this).addClass('active');
+	return false;
+});
+
+
+/* Popup
+===============================*/
+$('.js-popup').magnificPopup({type:'image'});
+
+
+
+});
